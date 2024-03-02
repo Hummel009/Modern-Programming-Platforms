388ae015941d0f78b166f18f5b73c97a40955f81
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-03-02 21:23:09 +0300

    Update Program.cs

--- appLab2/appFaker/src/main/csharp/com/github/hummel/mpp/lab2/Program.cs
+++ appLab2/appFaker/src/main/csharp/com/github/hummel/mpp/lab2/Program.cs
@@ -25,13 +25,10 @@ public class TestClass
 
     public int a;
     public int b;
-    private int t;
-    private int y;
     public A cA;
 
     public int c { get; private set; }
     public int d { private get; set; }
-    private int dg { get; set; }
 
     public List<int> aasds;
     public int aasd() { return 5; }
