51eca9a402193fd21e2b10126ab013f1fedc8b15
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-03-22 15:27:01 +0300

    Update Generator.cs

--- appLab4/appGenerator/src/main/csharp/com/github/hummel/mpp/lab4/Generator.cs
+++ appLab4/appGenerator/src/main/csharp/com/github/hummel/mpp/lab4/Generator.cs
@@ -4,7 +4,6 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp;
 using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.CSharp.Formatting;
 using Formatter = Microsoft.CodeAnalysis.Formatting.Formatter;
 using System.Collections.Concurrent;
