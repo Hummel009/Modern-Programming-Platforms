c2fe1dd6dd13a8af008eee8d0d2066aacc20e304
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-03-26 14:38:33 +0300

    Update DependencyConfig.cs

--- appLab5/appContainer/src/main/csharp/com/github/hummel/mpp/lab5/util/DependencyConfig.cs
+++ appLab5/appContainer/src/main/csharp/com/github/hummel/mpp/lab5/util/DependencyConfig.cs
@@ -6,16 +6,16 @@ public class DepConfig
 
     public List<CustomDependency> getDependencies() => new(dependencies);
 
-    public void register<TDependency, TImplementation>(Kind kind = Kind.INSTANCE_PER_DEPENDENCY, string? name = null, params DependencyParameter[] parameters)
+    public void register<DepType, ImplType>(Kind kind = Kind.INSTANCE_PER_DEPENDENCY, string? name = null, params DependencyParameter[] parameters)
     {
-        register(typeof(TDependency), typeof(TImplementation), kind, name, parameters);
+        register(typeof(DepType), typeof(ImplType), kind, name, parameters);
     }
 
     public void register(Type depType, Type implType, Kind kind = Kind.INSTANCE_PER_DEPENDENCY, string? name = null, params DependencyParameter[] parameters)
     {
         if (dependencies.Where(d => d.depType == depType && d.name == name).ToList().Count == 1)
         {
-            throw new Exception($"{depType.Name} with name {name} already exists");
+            throw new Exception($"Choose another name when registering implementations.");
         }
         dependencies.Add(new CustomDependency(depType, implType, kind, name, [.. parameters]));
     }
