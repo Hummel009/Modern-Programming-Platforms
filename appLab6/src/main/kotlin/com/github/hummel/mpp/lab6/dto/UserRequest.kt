26d66b5d91be7cd279da795ed67e35a75170197f
Hummel009 <hummel.turbamentum@gmail.com>
2025-05-29 18:06:05 +0300

    Update UserRequest.kt

--- appLab6/src/main/kotlin/com/github/hummel/mpp/lab6/dto/UserRequest.kt
+++ appLab6/src/main/kotlin/com/github/hummel/mpp/lab6/dto/UserRequest.kt
@@ -1,7 +1,3 @@
 package com.github.hummel.mpp.lab6.dto
 
-import com.github.hummel.mpp.lab6.entity.User
-
-data class UserRequest(val username: String, val password: String) {
-	fun toEntity(): User = User(username, password)
-}
+data class UserRequest(val username: String, val password: String)
